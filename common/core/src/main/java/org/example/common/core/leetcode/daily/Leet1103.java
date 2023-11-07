4a235785c9e32a089ee041ec518bfe76b52bcdde
lejun <lejun3389@qq.com>
2023-11-07 14:38:49 +0800

    滑动窗口的练习

--- common/core/src/main/java/org/example/common/core/leetcode/daily/Leet1103.java
+++ common/core/src/main/java/org/example/common/core/leetcode/daily/Leet1103.java
@@ -4,7 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.omg.CORBA.WCharSeqHelper;
 
-import java.util.HashMap;
+import java.util.*;
 
 /**
  * @Author: lejun
@@ -126,6 +126,149 @@ public class Leet1103 {
             }
         }
         return false;
+    }
+
+    /**
+     * 获取到最多2个不同字符的最长字串
+     * 1,d
+     * 2,滑动窗口。
+     *
+     * @param s
+     * @return
+     */
+    public String maxLenOfTwoChar1(String s) {
+        if (s == null || s.length() == 0) {
+            return "";
+        }
+        int n = s.length();
+        char[] chars = s.toCharArray();
+        /**
+         * i,j符合要求的最长  dp假设有1~2个
+         */
+        int[][] dp = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = 1;
+        }
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i + 1; j < n; j++) {
+            }
+        }
+        // 要求要字串。
+        return "";
+    }
+
+    /**
+     * <p>
+     * abbcccd
+     * 左右指针都需要重置和处理。这样要怎么做？
+     *
+     * </p>
+     *
+     * @param s
+     * @return
+     */
+    public int lengthOfLongestSubstringTwoDistinct(String s) {
+        char[] chars = s.toCharArray();
+        int len = chars.length;
+        Map<Character, Integer> map = new HashMap<>();
+        int left = 0, right = 0;
+        //不同的字符数量
+        int count = 0;
+        int maxLength = 0;
+        final HashMap<Integer, ArrayList<int[]>> memory = new HashMap<>();
+        while (right < len) {
+            //在map中获取该字符出现的次数(不存在则为0),次数+1
+            int rightNumber = map.getOrDefault(chars[right], 0) + 1;
+            //更新map
+            map.put(chars[right], rightNumber);
+            //第一次出现
+            if (rightNumber == 1) {
+                count++;
+            }
+            right++;
+            if (count <= 2) {
+                // 记录所有的结果。只要符合要求的都需要。
+                if (right == len -1) {
+                    log.info("left:{},right:{}", left, right);
+                }
+                if (right == len -2) {
+                    log.info("left:{},right:{}, str:{}", left, right, s.substring(left, right));
+                    maxLength = Math.max(maxLength, right - left);
+                    log.info("a0a1b2c3c4c5d6最大的长度{}", maxLength);
+                }
+                maxLength = Math.max(maxLength, right - left);
+                // 只有一个记录进来了。没有其他的？
+                final ArrayList<int[]> item = memory.getOrDefault(maxLength, new ArrayList<>());
+                item.add(new int[]{left, right});
+                memory.put(maxLength, item);
+            }
+            while (count > 2) {
+                //左侧移除一个字符，在map中获取该字符出现的次数-1
+                int leftNumber = map.get(chars[left]) - 1;
+                //更新map
+                map.put(chars[left], leftNumber);
+                if (leftNumber == 0) {
+                    count--;
+                }
+                left++;
+            }
+        }
+        // 记录下左右指针获取到字串？
+        final ArrayList<int[]> ints = memory.get(maxLength);
+        for (int[] anInt : ints) {
+            String rs = s.substring(anInt[0], anInt[1]+1);
+            log.info("{}", rs);
+        }
+        return maxLength;
+    }
+
+
+    @Test
+    public void testTwo() {
+        String s = "aabcccd";
+        System.out.println(findMaxTwoDistinctCharsSubstring(s));
+    }
+
+    public static List<String> findMaxTwoDistinctCharsSubstring(String s) {
+        if (s == null || s.length() == 0) {
+            return new ArrayList<>();
+        }
+
+        int left = 0;
+        int right = 0;
+        int maxLen = 0;
+        int distinctCount = 0;
+        Map<Character, Integer> charCount = new HashMap<>();
+        List<String> result = new ArrayList<>();
+
+        while (right < s.length()) {
+            char rightChar = s.charAt(right);
+            charCount.put(rightChar, charCount.getOrDefault(rightChar, 0) + 1);
+
+            while (charCount.size() > 2) {
+                char leftChar = s.charAt(left);
+                charCount.put(leftChar, charCount.get(leftChar) - 1);
+                if (charCount.get(leftChar) == 0) {
+                    charCount.remove(leftChar);
+                }
+                left++;
+            }
+
+            if (charCount.size() <= 2) {
+                int currentLen = right - left + 1;
+                if (currentLen > maxLen) {
+                    maxLen = currentLen;
+                    result.clear();
+                    result.add(s.substring(left, right + 1));
+                } else if (currentLen == maxLen) {
+                    result.add(s.substring(left, right + 1));
+                }
+            }
+
+            right++;
+        }
+
+        return result;
     }
 
 }

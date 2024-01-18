b55af9c9f5f5af7e1aa0579bd88bfeb11a5a0bd9
lejun <lejun3389@qq.com>
2024-01-18 15:59:37 +0800

    数位dp的题目

--- common/core/src/main/java/org/example/common/core/leetcode/twentyFour/January/Leet0116.java
+++ common/core/src/main/java/org/example/common/core/leetcode/twentyFour/January/Leet0116.java
@@ -2,7 +2,10 @@ package org.example.common.core.leetcode.twentyFour.January;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * @Author: lejun
@@ -142,9 +145,156 @@ public class Leet0116 {
     }
 
     /**
-     * 1.给定两个正整数 a,b，求在 [a,b] 中的所有整数中，每个数码（digit）各出现了多少次。
-     * 2.统计一个区间内数位上不能有 4 也不能有连续的 62 的数有多少。
-     * 3.给定一个区间 [l,r]，求其中满足条件不含前导 0且相邻两个数字相差至少为 2的数字个数。
+     * 788. 旋转数字（题解）
+     * 902. 最大为 N 的数字组合（题解）1990
+     * 233. 数字 1 的个数（题解）
+     * 面试题 17.06. 2 出现的次数（题解）
+     * 600. 不含连续 1 的非负整数（题解）
+     * 2376. 统计特殊整数（题解）2120
+     * 1012. 至少有 1 位重复的数字（题解）2230
+     * 357. 统计各位数字都不同的数字个数
+     * 2999. 统计强大整数的数目
+     * 2827. 范围中美丽整数的数目 2324
+     * 2801. 统计范围内的步进数字数目 2367
+     * 1397. 找到所有好字符串 2667
+     * 1215. 步进数（会员题）1675
+     * 1067. 范围内的数字计数（会员题）2025
+     * 1742. 盒子中小球的最大数量 *请使用非暴力做法解决
      */
+
+    static int[] check = {0,0,1,-1,-1,1,1,-1,0,1};
+    int[][][] memo = new int[5][2][2];
+    List<Integer> digits =  new ArrayList<Integer>();
+    public int rotateDigit(int num) {
+        while (num != 0) {
+            digits.add(num % 10);
+            num /= 10;
+        }
+        Collections.reverse(digits);
+
+        return 0;
+    }
+
+    @Test
+    public void testLessN() {
+        String[] digit = {"1","3","5","7"};
+        atMostNGivenDigitSet(digit, 100);
+    }
+
+    /**
+     * <href>902</href>
+     * <p>
+     *     1234   1==> (012)
+     *     0==> (0~9)
+     *
+     *     dfs要的是说i == chars[i] ? chars[i] : 9;
+     *     123  和 1123 ==>
+     *     <b>数位dp的模板数据</b>
+     * </p>
+     *
+     * @param digits
+     * @param n
+     * @return
+     */
+    public int atMostNGivenDigitSet(String[] digits, int n) {
+        this.digit = digits;
+        s = Integer.toString(n).toCharArray();
+        dp = new int[s.length];
+        Arrays.fill(dp, -1);
+        return f(0, true, false);
+    }
+
+    /**
+     * isLimit 是来判断出可选0~9 或者234的。
+     *
+     * @param i
+     * @param isLimit
+     * @param isNum
+     * @return
+     */
+    private int f(int i, boolean isLimit, boolean isNum) {
+        if (i == s.length){
+            return isNum ? 1 : 0;
+        }
+        // 原因是什么？ 不限制；是数字；大于0； isNum的意义是什么？
+        //
+        if (!isLimit && isNum && dp[i] > 0) {
+            return dp[i];
+        }
+        int res = 0;
+        if (!isNum) {
+            // 前面不填数字，那么可以跳过当前数位，也不填数字
+            // isLimit 改为 false，因为没有填数字，位数都比 n 要短，自然不会受到 n 的约束
+            // isNum 仍然为 false，因为没有填任何数字
+            res = f(i+1, false, false);
+        }
+        char up = isLimit ? s[i] : '9';
+        for (String d : digit) {
+            if (d.charAt(0) > up) {
+                break;
+            }
+            res += f(i+1, isLimit && d.charAt(0) == up, true);
+        }
+        if (!isLimit && isNum) {
+            // 在不受到任何约束的情况下，记录结果
+            dp[i] = res;
+        }
+        return res;
+    }
+
+    private String[] digit;
+    private char[] s;
+    private int[] dp;
+
+    /**
+     * 1001 ==> 1000
+     * 1000 ==> 099
+     *
+     * @param num
+     * @return
+     */
+    public String sub0118(String num) {
+        int len = num.length();
+        final char[] chars = num.toCharArray();
+        int i = len - 1;
+        while (num.charAt(i) == '0') {
+            i--;
+        }
+        chars[i]--;
+        i++;
+        while (i < len) {
+            chars[i++] = '9';
+        }
+        return new String(chars);
+    }
+
+    /**
+     * 1~n中1出现的个数。数位dp。
+     * 123
+     * 100
+     * 101
+     * 111
+     * 01 011 001.....
+     *
+     * @param n
+     * @return
+     */
+//    public int oneAppear(int n) {
+//
+////    }
+//
+//    /**
+//     * for_i : n:
+//     *  if (i == 1) {
+//     *      count++;
+//     *  }
+//     *
+//     * @param i
+//     * @param isLimit
+//     * @return
+//     */
+//    public int dfsOneAppear(int i, boolean isLimit) {
+//
+//    }
 
 }

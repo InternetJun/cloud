ef1bb3903cb566c52741ed0715aa74a1922f177d
lejun <lejun3389@qq.com>
2024-02-25 22:42:10 +0800

    括号用栈进行一个处理

--- common/core/src/main/java/org/example/common/core/leetcode/twentyFour/febuary/Hot100.java
+++ common/core/src/main/java/org/example/common/core/leetcode/twentyFour/febuary/Hot100.java
@@ -725,4 +725,70 @@ public class Hot100 {
         final List<Integer> list = Arrays.asList(1, 2, 3, 3, 4);
         System.out.println(binarySearch(list, 3));
     }
+
+    /**
+     * 最长的有效括号。和连续子数组很像啊。
+     *
+     * @param s
+     * @return
+     */
+    public int longestValidParentheses(String s) {
+        Stack<Integer> stk = new Stack<>();
+        // dp[i] 的定义：记录以 s[i-1] 结尾的最长合法括号子串长度
+        int[] dp = new int[s.length() + 1];
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                // 遇到左括号，记录索引
+                stk.push(i);
+                // 左括号不可能是合法括号子串的结尾
+                dp[i + 1] = 0;
+            } else {
+                // 遇到右括号
+                if (!stk.isEmpty()) {
+                    // 配对的左括号对应索引
+                    int leftIndex = stk.pop();
+                    // 以这个右括号结尾的最长子串长度
+                    int len = 1 + i - leftIndex + dp[leftIndex];
+                    dp[i + 1] = len;
+                } else {
+                    // 没有配对的左括号
+                    dp[i + 1] = 0;
+                }
+            }
+        }
+        // 计算最长子串的长度
+        int res = 0;
+        for (int i = 0; i < dp.length; i++) {
+            res = Math.max(res, dp[i]);
+        }
+        return res;
+    }
+
+    public boolean isValid(String str) {
+        Stack<Character> left = new Stack<>();
+        for (char c : str.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                left.push(c);
+            } else // 字符 c 是右括号
+                if (!left.isEmpty() && leftOf(c) == left.peek()) {
+                    left.pop();
+                } else
+                    // 和最近的左括号不匹配
+                {
+                    return false;
+                }
+        }
+        // 是否所有的左括号都被匹配了
+        return left.isEmpty();
+    }
+
+    char leftOf(char c) {
+        if (c == '}') {
+            return '{';
+        }
+        if (c == ')') {
+            return '(';
+        }
+        return '[';
+    }
 }

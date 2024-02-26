1eb286b4e56afcfa0df523b9d0ef2b6c1d17ae52
lejun <lejun3389@qq.com>
2024-02-26 14:23:34 +0800

    继续几个有趣的sql需求

--- common/core/src/main/java/org/example/common/core/leetcode/twentyFour/febuary/test.sql
+++ common/core/src/main/java/org/example/common/core/leetcode/twentyFour/febuary/test.sql
@@ -48,3 +48,53 @@ FROM
 WHERE
         @num < 6;
 
+-- 连续出现3次的数字。
+/**
+  在表中需要连续出现，需要有窗口函数。
+  Logs
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| num         | varchar |
++-------------+---------+
+ */
+with tmp as
+         (select s.*, row_number()over(order by visit_date) r, id-row_number()over(order by visit_date) rr
+          from Stadium s
+          where people>=100)
+select id, visit_date, people
+from tmp
+where rr in (select rr from tmp group by rr having count(rr)>=3);
+-- 要是有连续出现的数字，则有的sql是
+select distinct p.num as ConsecutiveNums
+from (
+         select id,
+                num,
+                -- lag函数的意思是取值到前几行。还有对应的lead()函数：取值后几行。
+                lag(num, 1) over(order by id) num1, lag(num, 2) over(order by id) num2
+         from Logs
+     ) p
+where p.num = p.num1
+  and p.num1 = p.num2
+
+-- 在表中有如下的结构。
+/**
+  +-------------+------+
+| Column Name | Type |
++-------------+------+
+| id          | int  |
+| p_id        | int  |
++-------------+------+
+id 是该表中具有唯一值的列。;
+存储的是id和对应的父节点。所以要怎么对每个节点都确定为什么类型的呢？
+  判断的依据是什么呢？
+ */
+select id,
+       Case
+           When p_id is null Then "Root"
+           When id in (select distinct p_id from tree) Then "Inner"
+           Else "Leaf"
+       End Type
+from tree;
+-- exists一般是用于where后面的条件。不要使用在case when中了。

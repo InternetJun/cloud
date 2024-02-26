079a7bd21128b99dffd846c7701a883c08a73a92
lejun <lejun3389@qq.com>
2024-02-26 15:08:34 +0800

    感染源的四处发散的广度优先遍历算法

--- common/core/src/main/java/org/example/common/core/leetcode/twentyFour/febuary/Hot100.java
+++ common/core/src/main/java/org/example/common/core/leetcode/twentyFour/febuary/Hot100.java
@@ -791,4 +791,151 @@ public class Hot100 {
         }
         return '[';
     }
+
+    /**
+     * 通过率65/85；
+     * @param root
+     * @return
+     */
+    public boolean isValidBST(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        if (root != null) {
+            if ((root.right != null && root.right.val <= root.val) ||
+                    (root.left != null && root.left.val >= root.val)) {
+                return false;
+            }
+            return true;
+        }
+        return isValidBST(root.left) && isValidBST(root.right);
+    }
+
+    public boolean isValidBSTSolution(TreeNode root) {
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    /**
+     * 是有一个上下限的，对此节点进行一个判断就好了。
+     *
+     * @param node
+     * @param lower
+     * @param upper
+     * @return
+     */
+    public boolean isValidBST(TreeNode node, long lower, long upper) {
+        if (node == null) {
+            return true;
+        }
+        if (node.val <= lower || node.val >= upper) {
+            return false;
+        }
+        return isValidBST(node.left, lower, node.val) && isValidBST(node.right, node.val, upper);
+    }
+
+    /**
+     * 获取到第k个数。
+     * <p>
+     *  思路是左右数的二叉遍历吧。
+     * </p>
+     * @param root
+     * @param k
+     * @return
+     */
+    public int kthSmallest(TreeNode root, int k) {
+        // 利用 BST 的中序遍历特性
+        traverse(root, k);
+        return result;
+    }
+
+    // 记录结果
+    int result = 0;
+    // 记录当前元素的排名
+    int rank = 0;
+    void traverse(TreeNode root, int k) {
+        if (root == null) {
+            return;
+        }
+        traverse(root.left, k);
+        /* 中序遍历代码位置 */
+        rank++;
+        if (k == rank) {
+            // 找到第 k 小的元素
+            result = root.val;
+            return;
+        }
+        /*****************/
+        traverse(root.right, k);
+    }
+
+    /**
+     * <p>
+     * [[2,1,1],[0,1,1],[1,0,1]]
+     * X O O
+     * [ O O
+     * O [ O
+     * 什么情况来说明是可以说明就全部感染了呢？用count？
+     * 【0 2】说明就全部感染了啊。
+     * 要是有多个感染源呢？==就是用count的。
+     * </p>
+     *
+     * @param grid
+     * @return
+     */
+    public int orangesRotting(int[][] grid) {
+        //1.定义2个int数组，2个一组来记录腐烂橘子的上下左右位置。腐烂橘子(0，0)
+        //在矩阵中 上{-1,0}   下{1,0}  左{0,-1}   右{0,1}
+        int[] dx = {-1, 1, 0, 0};
+        int[] dy = {0, 0, -1, 1};
+        int step = 0;//感染次数
+        int flash = 0;//新鲜橘子数（后面用于判定是否为-1）
+
+        int row = grid.length;//所给矩阵行
+        int col = grid[0].length;//列
+
+        Queue<int[]> queue = new ArrayDeque<>();
+        //2.遍历矩阵 将所有的腐烂橘子入队，并且记录初始新鲜橘子数
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (grid[i][j] == 1) {
+                    flash++;
+                }
+                if (grid[i][j] == 2) {
+                    queue.offer(new int[]{i, j});
+                }
+            }
+        }
+
+        //3.遍历所有腐烂橘子，同时感染四周
+        while (flash > 0 && !queue.isEmpty()) {//有橘子且队列不空
+            step++;
+            //队列中现有的所有腐烂橘子都要进行一次感染
+            int size = queue.size();
+            for (int s = 0; s < size; s++) {
+                int[] poll = queue.poll();//腐烂橘子
+                for (int i = 0; i < 4; i++) {
+                    //4个位置dx[i] dy[i]  ， xy 为要感染的橘子位置
+                    int x = poll[0] + dx[i];//第x行
+                    int y = poll[1] + dy[i];//第y列
+                    if ((x >= 0 && x < row) && (y >= 0 && y < col) && grid[x][y] == 1) {
+                        //xy不越界，并且要感染的地方是 新鲜橘子
+                        grid[x][y] = 2;
+                        //把被感染的橘子 入队
+                        queue.offer(new int[]{x, y});
+                        //新鲜橘子-1
+                        flash--;
+                    }
+                }
+            }
+
+        }
+
+        //感染完了之后如果还有新鲜橘子
+        if (flash > 0) {
+            return -1;
+        } else {
+            return step;
+        }
+    }
+
 }

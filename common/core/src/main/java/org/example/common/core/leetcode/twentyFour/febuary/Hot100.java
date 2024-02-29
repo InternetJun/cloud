36db38406270f8c8a9c8c22d6fa3e0fc9225cccb
lejun <lejun3389@qq.com>
2024-02-29 15:33:03 +0800

    链表的处理，分治算法

--- common/core/src/main/java/org/example/common/core/leetcode/twentyFour/febuary/Hot100.java
+++ common/core/src/main/java/org/example/common/core/leetcode/twentyFour/febuary/Hot100.java
@@ -209,54 +209,6 @@ public class Hot100 {
         return dummy.next;
     }
 
-    public ListNode sortList(ListNode head) {
-        return sortList(head, null);
-    }
-
-    public ListNode sortList(ListNode head, ListNode tail) {
-        if (head == null) {
-            return head;
-        }
-        if (head.next == tail) {
-            head.next = null;
-            return head;
-        }
-        ListNode slow = head, fast = head;
-        while (fast != tail) {
-            slow = slow.next;
-            fast = fast.next;
-            if (fast != tail) {
-                fast = fast.next;
-            }
-        }
-        ListNode mid = slow;
-        ListNode list1 = sortList(head, mid);
-        ListNode list2 = sortList(mid, tail);
-        ListNode sorted = merge(list1, list2);
-        return sorted;
-    }
-
-    public ListNode merge(ListNode head1, ListNode head2) {
-        ListNode dummyHead = new ListNode(0);
-        ListNode temp = dummyHead, temp1 = head1, temp2 = head2;
-        while (temp1 != null && temp2 != null) {
-            if (temp1.val <= temp2.val) {
-                temp.next = temp1;
-                temp1 = temp1.next;
-            } else {
-                temp.next = temp2;
-                temp2 = temp2.next;
-            }
-            temp = temp.next;
-        }
-        if (temp1 != null) {
-            temp.next = temp1;
-        } else if (temp2 != null) {
-            temp.next = temp2;
-        }
-        return dummyHead.next;
-    }
-
     /**
      * 两两的节点交换。有什么的需求？
      *
@@ -1314,5 +1266,110 @@ public class Hot100 {
             slow = slow.next;
         }
         return slow;
+    }
+
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists== null || lists.length == 0) {
+            return null;
+        }
+        ListNode node = null;
+        // 分支的算法。
+        int size = lists.length-1;
+        return mergeHelper(lists, 0, size);
+    }
+
+    private ListNode mergeHelper(ListNode[] lists, int left, int right) {
+        if (right == left) {
+            return lists[left];
+        }
+        int mid = left + (right - left) / 2;
+        final ListNode node = mergeHelper(lists, left, mid);
+        final ListNode node1 = mergeHelper(lists, mid + 1, right);
+        return mergeTwoListNode(node1, node);
+    }
+
+    /**
+     * 合并2个有序链表。
+     *
+     * @param left
+     * @param right
+     * @return
+     */
+    public ListNode mergeTwoListNode(ListNode left, ListNode right) {
+        ListNode dummy = new ListNode(-1), dummyPoint = dummy;
+        ListNode curL = left, curR = right;
+        while (curL != null && curR != null) {
+            if (curL.val >= curR.val) {
+                dummyPoint.next = curR;
+                curR = curR.next;
+            } else {
+                dummyPoint.next = curL;
+                curL = curL.next;
+            }
+            dummyPoint = dummyPoint.next;
+        }
+        if (curL != null) {
+            dummyPoint.next = curL;
+        }
+        if (curR != null) {
+            dummyPoint.next = curR;
+        }
+        return dummy.next;
+    }
+
+    @Test
+    public void testMerge() {
+        final ListNode node = CommonUtil.buildLinkedList(new int[]{1, 3, 5});
+        final ListNode node1 = CommonUtil.buildLinkedList(new int[]{2, 4, 6});
+        final ListNode node2 = mergeTwoListNode(node1, node);
+        log.info("{}", node2);
+    }
+
+    public ListNode sortList(ListNode head) {
+        return sortList(head, null);
+    }
+
+    public ListNode sortList(ListNode head, ListNode tail) {
+        if (head == null) {
+            return head;
+        }
+        if (head.next == tail) {
+            head.next = null;
+            return head;
+        }
+        ListNode slow = head, fast = head;
+        while (fast != tail) {
+            slow = slow.next;
+            fast = fast.next;
+            if (fast != tail) {
+                fast = fast.next;
+            }
+        }
+        ListNode mid = slow;
+        ListNode list1 = sortList(head, mid);
+        ListNode list2 = sortList(mid, tail);
+        ListNode sorted = merge(list1, list2);
+        return sorted;
+    }
+
+    public ListNode merge(ListNode head1, ListNode head2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode temp = dummyHead, temp1 = head1, temp2 = head2;
+        while (temp1 != null && temp2 != null) {
+            if (temp1.val <= temp2.val) {
+                temp.next = temp1;
+                temp1 = temp1.next;
+            } else {
+                temp.next = temp2;
+                temp2 = temp2.next;
+            }
+            temp = temp.next;
+        }
+        if (temp1 != null) {
+            temp.next = temp1;
+        } else if (temp2 != null) {
+            temp.next = temp2;
+        }
+        return dummyHead.next;
     }
 }

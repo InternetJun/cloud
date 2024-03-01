faa89271c21ac6db085b9ca4df84f4a8e8e049ee
lejun <lejun3389@gmail.com>
2024-03-01 16:27:36 +0800

    下一列排列和排序012的快排

--- common/core/src/main/java/org/example/common/core/leetcode/twentyFour/febuary/Hot100.java
+++ common/core/src/main/java/org/example/common/core/leetcode/twentyFour/febuary/Hot100.java
@@ -1634,4 +1634,84 @@ public class Hot100 {
     int min(int a, int b, int c) {
         return Math.min(a, Math.min(b, c));
     }
+
+    public void sortColors(int[] nums) {
+        int len = nums.length;
+        if (len < 2) {
+            return;
+        }
+
+        // all in [0, zero) = 0
+        // all in [zero, i) = 1
+        // all in [two, len - 1] = 2
+
+        // 循环终止条件是 i == two，那么循环可以继续的条件是 i < two
+        // 为了保证初始化的时候 [0, zero) 为空，设置 zero = 0，
+        // 所以下面遍历到 0 的时候，先交换，再加
+        int zero = 0;
+
+        // 为了保证初始化的时候 [two, len - 1] 为空，设置 two = len
+        // 所以下面遍历到 2 的时候，先减，再交换
+        int two = len;
+        int i = 0;
+        // 当 i == two 上面的三个子区间正好覆盖了全部数组
+        // 因此，循环可以继续的条件是 i < two
+        while (i < two) {
+            if (nums[i] == 0) {
+                swap(nums, i, zero);
+                zero++;
+                i++;
+            } else if (nums[i] == 1) {
+                i++;
+            } else {
+                two--;
+                swap(nums, i, two);
+            }
+        }
+    }
+
+    private void swap(int[] nums, int index1, int index2) {
+        int temp = nums[index1];
+        nums[index1] = nums[index2];
+        nums[index2] = temp;
+    }
+
+    /**
+     * <p>
+     * 我们结合样例来分析，假设样例为 [1,3,5,4,1]：
+     *
+     * 从后往前找，找到第一个下降的位置，记为 k。注意k 以后的位置是降序的。 在样例中就是找到 【3】
+     *
+     * 从 k 往后找，找到最小的比 k 要大的数。 找到 【4】
+     *
+     * 将两者交换。注意此时 k 以后的位置仍然是降序的。
+     *
+     * 直接将 k 以后的部分翻转（变为升序）。
+
+     * </p>
+     *
+     * @param nums
+     */
+    public void nextPermutation(int[] nums) {
+        int n = nums.length, k = n - 1;
+        while (k - 1 >= 0 && nums[k - 1] >= nums[k]) {
+            k--;
+        }
+        if (k == 0) {
+            reverse(nums, 0, n - 1);
+        } else {
+            int u = k;
+            while (u + 1 < n && nums[u + 1] > nums[k - 1]) {
+                u++;
+            }
+            swap(nums, k - 1, u);
+            reverse(nums, k, n - 1);
+        }
+    }
+    void reverse(int[] nums, int a, int b) {
+        int l = a, r = b;
+        while (l < r) {
+            swap(nums, l++, r--);
+        }
+    }
 }

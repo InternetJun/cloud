a88a88d197a077f214b50dfb511574899e6b48bf
lejun <lejun3389@gmail.com>
2024-03-04 11:23:42 +0800

    打家劫舍的动态规划问题

--- common/core/src/main/java/org/example/common/core/leetcode/twentyFour/febuary/Hot100.java
+++ common/core/src/main/java/org/example/common/core/leetcode/twentyFour/febuary/Hot100.java
@@ -2008,4 +2008,58 @@ public class Hot100 {
             track.removeLast();
         }
     }
+
+    /**
+     * dp[i] = dp[i-1] + nums[i];
+     *
+     * @param nums
+     * @return
+     */
+    public int rob (int[] nums) {
+        if(nums == null || nums.length == 0) {
+            return 0;
+        }
+        // write code here
+        int n = nums.length;
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+        dp[1] = nums[0];
+        for(int i = 2; i <= n; i++) {
+            dp[i] = Math.max(dp[i-1], dp[i-2] + nums[i-1]);
+        }
+        return dp[n];
+    }
+
+    /**
+     * 环形的数组。可以有n 和 1是连起来的。
+     * dp[n] = dp[n-1], dp[i-2]+nums[i-1];
+     *
+     * @param nums
+     * @return
+     */
+    public int robII(int[] nums) {
+        int length = nums.length;
+        if (length == 1) {
+            return nums[0];
+        } else if (length == 2) {
+            return Math.max(nums[0], nums[1]);
+        }
+        return Math.max(robRange(nums, 0, length - 2), robRange(nums, 1, length - 1));
+    }
+
+    public int robRange(int[] nums, int start, int end) {
+        int first = nums[start], second = Math.max(nums[start], nums[start + 1]);
+        for (int i = start + 2; i <= end; i++) {
+            int temp = second;
+            second = Math.max(first + nums[i], second);
+            first = temp;
+        }
+        return second;
+    }
+
+    @Test
+    public void mainRob(){
+        int[] nums = {1,2,3,4};
+        System.out.println(rob(nums));
+    }
 }

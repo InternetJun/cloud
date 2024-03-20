5de9bf7cb05e4b630d67db0ed228b3833f7448e1
lejun <lejun3389@gmail.com>
2024-03-20 16:57:19 +0800

    岛屿的周长，定义为0、边界

--- common/core/src/main/java/org/example/common/core/leetcode/graph/Graph.java
+++ common/core/src/main/java/org/example/common/core/leetcode/graph/Graph.java
@@ -115,5 +115,29 @@ public class Graph {
                         dfsArea(grid, c, r - 1) + 1;
     }
 
+    // 岛屿的周长的
+    static int[] dx = {0, 1, 0, -1};
+    static int[] dy = {1, 0, -1, 0};
+
+    public int islandCicle(int[][] grid) {
+        int n = grid.length, m = grid[0].length;
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                if (grid[i][j] == 1) {
+                    int cnt = 0;
+                    for (int k = 0; k < 4; ++k) {
+                        int tx = i + dx[k];
+                        int ty = j + dy[k];
+                        if (tx < 0 || tx >= n || ty < 0 || ty >= m || grid[tx][ty] == 0) {
+                            cnt += 1;
+                        }
+                    }
+                    ans += cnt;
+                }
+            }
+        }
+        return ans;
+    }
 
 }
